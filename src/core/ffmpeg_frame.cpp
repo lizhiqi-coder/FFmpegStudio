989af9ccaa5326d538f750b2a89f6f2bc393e574
lizhiqi <chinalizhiqi@gmail.com>
2017-09-22 00:47:41 +0800

    add frame

---
+++ src/core/ffmpeg_frame.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 58boy on 2017/9/22.
+//
+
+#include "ffmpeg_frame.h"
---
+++ src/core/ffmpeg_frame.h
@@ -0,0 +1,75 @@
+//
+// Created by 58boy on 2017/9/22.
+//
+
+#ifndef FFMPEGSTUDIO_FFMPEG_FRAME_H
+#define FFMPEGSTUDIO_FFMPEG_FRAME_H
+
+class FFImage {
+public:
+    int width;
+    int height;
+    int width_step;
+    unsigned char *image_data;//字节数组
+    int image_size;
+    int n_channels;
+    int pix_type;
+    int image_orientation;
+    bool is_image_square;
+
+    FFImage() {
+        image_orientation = 0;
+        is_image_square = false;
+    }
+
+    FFImage(int w, int h, int pix_t, int channels) {
+        width = w;
+        height = h;
+        pix_type = pix_t;
+        n_channels = channels;
+        width_step = width * n_channels;
+        image_size = width * width_step;
+        image_data = 0;
+        image_orientation = 0;
+        is_image_square = false;
+    }
+
+    static inline FFImage *createHeader(int w, int h, int pix_t, int channels) {
+        return new FFImage(w, h, p, channels);
+    }
+};
+
+class Frame {
+public:
+    bool key_fame;
+    long timestamp;
+    long start_timestamp;
+    FFImage *image;
+
+    /**
+     * audio
+     */
+//samples
+    int nb_samples;
+    int sample_rate;
+    int sample_format;
+    int sample_channels;
+    long size;
+
+    Frame(){
+        key_fame = false;
+        image = 0;
+        nb_samples = 0;
+        timestamp = 0;
+        start_timestamp = 0;
+        size = 0;
+        sample_rate = 0;
+        sample_format = -1;
+        sample_channels = 0;
+    }
+
+
+};
+
+
+#endif //FFMPEGSTUDIO_FFMPEG_FRAME_H
